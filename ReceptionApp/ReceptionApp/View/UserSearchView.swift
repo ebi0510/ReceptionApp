47c8d6c2ec56970aac0b24af499593edd08ced0c
ebi0510 <yasuda.haruna.hk@gmail.com>
2026-03-17 23:17:06 +0900

    検索画面の実装

--- ReceptionApp/ReceptionApp/View/UserSearchView.swift
+++ ReceptionApp/ReceptionApp/View/UserSearchView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct UserSearchView: View {
+    @State private var searchText: String = ""
+    
+    let colors: [String] = ["Red", "Blue", "Yellow", "Purple", "Orange", "Green", "Pink", "White", "Gray", "Black"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
+        NavigationView {
+            List {
+                ForEach(filteredColors, id: \.self) { color in
+                    Text(color)
+                }
+            }
+            .navigationTitle("Colors")
+        }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
     }
+    
+    var filteredColors: [String] {
+        if searchText.isEmpty {
+            // 空欄の場合は、すべての候補を表示
+            return colors
+        } else {
+            // 条件に当てはまるアイテムがある場合は当てはまるものをすべて表示
+            return colors.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
 }
+
 
 #Preview {
     UserSearchView()
 }
+
+//参考：https://qiita.com/yama__0808/items/9f88a2f0a340095b073a
