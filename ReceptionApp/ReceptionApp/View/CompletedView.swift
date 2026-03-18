a9b39dd429db2e28cbabf54d3e8497d955987231
ebi0510 <yasuda.haruna.hk@gmail.com>
2026-03-18 23:13:36 +0900

    受付完了画面追加

---
+++ ReceptionApp/ReceptionApp/View/CompletedView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedView.swift
+//  ReceptionApp
+//
+//  Created by 岡野春菜 on 2026/03/18.
+//
+
+import SwiftUI
+
+struct CompletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedView()
+}
